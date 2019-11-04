fb32587726dd4cba6ba33390de93291202e261cf
wwwjjjnnn <1194348056@qq.com>
2019-11-04 11:01:11 +0800

    update main.

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import torch
 import torch.nn as nn
 from torch.optim import Adam
 import os
-from net_1 import Decoder, Encoder, Transformer
+from net import Decoder, Encoder, Net
 from path import MODEL_PATH, LOG_PATH
 from flyai.dataset import Dataset
 from model import Model
-import json
 
 # 超参
 parser = argparse.ArgumentParser()
+# 训练轮数
 parser.add_argument("-e", "--EPOCHS", default=10, type=int, help="train epochs")
+# 训练批次
 parser.add_argument("-b", "--BATCH", default=16, type=int, help="batch size")
 args = parser.parse_args()
 
@@ -21,64 +22,69 @@ if torch.cuda.is_available():
     device = 'cuda'
 else:
     device = 'cpu'
-
 device = torch.device(device)
-
 os.makedirs(MODEL_PATH, exist_ok=True)
 os.makedirs(LOG_PATH, exist_ok=True)
 
 # 数据获取辅助类
 data = Dataset(epochs=args.EPOCHS, batch=args.BATCH, val_batch=args.BATCH)
-# en = Encoder(20, 64)
-# de = Decoder(3507, 20, 64)
-
-network = Transformer(n_src_vocab=3507, n_tgt_vocab=3507, len_max_seq=64)
+# (embedding_dim, hidden_dim) eg.(20, 64)
+en = Encoder(20, 64)
+# (output_dim, embedding_dim, hidden_dim) eg.(3507, 20, 64)
+de = Decoder(3507, 20, 64)
+network = Net(en, de, device)
 loss_fn = nn.CrossEntropyLoss()
-
 optimizer = Adam(network.parameters())
-
 model = Model(data)
 iteration = 0
-
 lowest_loss = 10
+
 # 得到训练和测试的数据
 for i in range(data.get_step()):
+    # 开启训练状态
     network.train()
 
     # 得到训练和测试的数据
-    x_train, y_train = data.next_train_batch()  # 读取数据; shape:(sen_len,batch,embedding)
-    x_test, y_test = data.next_validation_batch()  # 读取数据; shape:(sen_len,batch,embedding)
-
+    # X_train: shape:(batch, sen_len, embedding) eg.(16, 901, 20)
+    # y_train: shape:(batch, 语音文字最大长度) eg.(16, 39)
+    x_train, y_train = data.next_train_batch()
+    # 读取数据; shape:(batch, sen_len, embedding)
+    x_test, y_test = data.next_validation_batch()
+    # batch_size eg.(16)
     batch_len = y_train.shape[0]
+    # seq_len中的最后一行 标记当前seq_len中非零的长度（真实的seq_len） # 转换为int
+    input_lengths = [int(x) for x in x_train[:, -1, 0].tolist()]
+    # 每段文字长度 eg.(6, 即该段语音对应的中文长度为6)
+    y_lengths = y_train[:, -1].tolist()
 
-    input_lengths = x_train[:, -1, 0]
-    input_lengths = input_lengths.tolist()
+    # 除去长度信息 eg.(16, 900, 20)
+    x_train = x_train[:, :-1, :]
+    # 除去长度信息 eg.(16, 38)
+    y_train = y_train[:, :-1]
 
-    input_lengths = [int(x) for x in input_lengths]
-    y_lengths = y_train[:, -1]
-    y_lengths = y_lengths.tolist()
+    # shape:(batch,sen_len,embedding)
+    x_train = torch.from_numpy(x_train).float().to(device)
+    # shape:(batch,sen_len)
+    y_train = torch.from_numpy(y_train).long().to(device)
 
-    x_train = x_train[:, :-1, :]  ## 除去长度信息
-    x_train = torch.from_numpy(x_train)  # shape:(batch,sen_len,embedding)
-    x_train = x_train.float().to(device)
-    y_train = y_train[:, :-1]  ## 除去长度信息
-    y_train = torch.from_numpy(y_train)  # shape:(batch,sen_len)
-    y_train = torch.LongTensor(y_train)
-    y_train = y_train.to(device)
-
+    # 依据input_length 进行从大到小的排序
     seq_pairs = sorted(zip(x_train.contiguous(), y_train.contiguous(), input_lengths, y_lengths), key=lambda x: x[2],
                        reverse=True)
+    # 皆为tuple类型
     x_train, y_train, input_lengths, y_lengths = zip(*seq_pairs)
+    # permute：更换tensor的维度 x_train: eg.(900, 16, 20)
     x_train = torch.stack(x_train, dim=0).permute(1, 0, 2).contiguous()
+    # y_train: eg.(38, 16)
     y_train = torch.stack(y_train, dim=0).permute(1, 0).contiguous()
     input_length_tensor = torch.tensor(input_lengths).long()
     y_lengths = torch.tensor(y_lengths).long()
-
-    outputs = Transformer(n_src_vocab=3507, n_tgt_vocab=3507, len_max_seq=64)
-
+    # x_train: eg.(900, 16, 20) input_lengths: eg.(16) y_train: eg.(38, 16)
+    outputs = network(x_train, input_lengths, y_train)
+    # 梯度清零
     optimizer.zero_grad()
+    # output: eg.(38, 16, 3507)
     outputs = outputs.float()
-
+    # outputs.view(-1, outputs.shape[2]): eg.(38*16, 3507)    y_train.view(-1): eg.(38*16)
     loss = loss_fn(outputs.view(-1, outputs.shape[2]), y_train.view(-1))
 
     # backward transmit loss
