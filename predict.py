c5577b09c7950a45fe517d5c5ff84c4f24842d92
wwwjjjnnn <1194348056@qq.com>
2019-11-05 00:02:10 +0800

    update.

--- predict.py
+++ predict.py
@@ -9,5 +9,12 @@ from path import MODEL_PATH
 
 data = Dataset()
 model = Model(data)
-p = model.predict(MODEL_PATH, upper='竹 杖 芒 鞋 轻 胜 马')
-print(p)
+# p = model.predict(MODEL_PATH, upper='竹 杖 芒 鞋 轻 胜 马')
+# print(p)
+
+q = model.predict_all(
+    datas=[{'audio_path':
+           "/data/2019_Form/2019-FlyAI-Life-Scene-Chinese-Speech-Recognition/data/input/wav/common_voice_zh-CN_18531674.wav"}]
+)
+
+print(q)
